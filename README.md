193fa7061dd5c8ae760196ec95898a170e2b501b
Chirag Jain <jainchg@gmail.com>
2019-02-04 01:16:19 +0530

    JSON Response extracted using OMDb API

--- README.md
+++ README.md
@@ -17,4 +17,12 @@ obtain movie information from IMDb.
 4. Movietime states that "These datasets will change over time, and are not appropriate for reporting research results."
 5. Since this dataset is sufficient for a proof of concept, we will use this dataset only.
 6. Dataset Used: http://files.grouplens.org/datasets/movielens/ml-latest-small-README.html
-7. Data presetn at /data/movieRatings
+7. Original Data present at /scripts/data/movieRatings
+
+### Modification of Dataset
+1. The movieset which we are using has a lot of data which we don't need so we have to extract the required data from the dataset.
+2. The modification of dataset is done in the script modifyData.py present under /scripts
+3. generateIMDBLinks() function extracts the movieID (for OMDb Dataset) and IMDbID for all movies present in the dataset which were released in the years 2015, 2016, 2017 or 2018.
+4. generateRatings() function extracts the ratings of multiple users who rated the movies which were extracted using the previous function.
+5. This data works as our data for dummy users which will be used in the Collaborative Filtering algorithm.
+6. getDetailsFromID() function results in the JSON response for each movie's data from the OMDb website.
--- scripts/modifyData.py
+++ scripts/modifyData.py
@@ -10,7 +10,7 @@ def generateIMDBLinks():
 			if '2015' in row[1] or '2016' in row[1] or '2017' in row[1] or '2018' in row[1]:
 				movieIDs.append(row[0])
 			line_count += 1
-	with open('data/movieRatings/movieIDs2.csv', mode='w') as movieID_file:
+	with open('data/movieRatings/movieIDs.csv', mode='w') as movieID_file:
 		movieID_writer = csv.writer(movieID_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 		with open('data/movieRatings/originalData/links.csv') as csv_file:
 			csv_reader = csv.reader(csv_file, delimiter=',')
@@ -27,7 +27,7 @@ def generateIMDBLinks():
 
 def generateRatings():
 	movieIDtoIMDbID = {}
-	with open('data/movieRatings/movieIDs2.csv') as movieID_file:
+	with open('data/movieRatings/movieIDs.csv') as movieID_file:
 		movieID_reader = csv.reader(movieID_file, delimiter=',')
 		line_count = 0
 		for row in movieID_reader:
@@ -37,7 +37,7 @@ def generateRatings():
 				movieIDtoIMDbID[row[0]] = row[1]
 				line_count += 1		
 
-	with open('data/movieRatings/ratings2.csv', mode='w') as ratings_file:
+	with open('data/movieRatings/ratings.csv', mode='w') as ratings_file:
 		ratings_writer = csv.writer(ratings_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 		with open('data/movieRatings/originalData/ratings.csv') as csv_file:
 			csv_reader = csv.reader(csv_file, delimiter=',')
@@ -63,7 +63,7 @@ def getDetailsFromID():
 			else:
 				movieIDs.append(row[1])
 				line_count += 1
-	with open('data/movieRatings/movieData2.csv', mode='w') as movieData_file:
+	with open('data/movieRatings/movieData.csv', mode='w') as movieData_file:
 		movieData_writer = csv.writer(movieData_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 		for i in range(len(movieIDs)):
 			print(i)
